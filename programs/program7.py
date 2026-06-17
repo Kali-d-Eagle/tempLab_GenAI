6d877050ab4e91293257c1911be53d43dc639bfc
Kali-d-Eagle <140918756+Kali-d-Eagle@users.noreply.github.com>
2026-06-18 01:16:59 +0530

    Update program7.py

--- programs/program7.py
+++ programs/program7.py
@@ -1,15 +1,18 @@
-from transformers import pipeline
+
+!pip install -U transformers==4.45.2
+
+from transformers import pipeline  
 
 
 summarizer = pipeline("summarization", model="t5-small")
 
+
 text = """
-The Industrial Revolution changed society from farming-based to industrial.
-Factories, machines, and mass production increased manufacturing and transportation.
+The Industrial Revolution, which took place from the 18th to the 19th centuries, was a period during which predominantly agrarian, rural societies in Europe and America became industrial and urban. Prior to the Industrial Revolution, manufacturing was often done in people's homes, using hand tools or basic machines. Industrialization marked a shift to powered, special-purpose machinery, factories and mass production. The iron and textile industries, along with the development of the steam engine, played central roles in the Industrial Revolution, which also saw improved systems of transportation, communication and banking. While industrialization brought about an increased volume and variety of manufactured goods and an improved standard of living for some, it also resulted in often grim employment and living conditions for the poor and working classes.
 """
 
 
-summary = summarizer(text, max_length=30, min_length=10, do_sample=False)
+summary = summarizer(text, max_length=60, min_length=30, do_sample=False)
 
 
 print(summary[0]['summary_text'])
