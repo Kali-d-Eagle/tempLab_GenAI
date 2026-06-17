e5fd460c66033ec9a48b285546acd3ba16387fac
Kali-d-Eagle <140918756+Kali-d-Eagle@users.noreply.github.com>
2026-06-18 00:19:37 +0530

    Update program6.py

--- programs/program6.py
+++ programs/program6.py
@@ -1,6 +1,6 @@
 from transformers import pipeline
 
-# Initialize Hugging Face sentiment-analysis pipeline
+
 sentiment = pipeline('sentiment-analysis')
 
 texts = [
@@ -8,10 +8,10 @@ texts = [
     "Worst product ever."
 ]
 
-# Run sentiment inference on list of texts
+
 results = sentiment(texts)
 
-# Print the text and its corresponding sentiment result
+
 for text, result in zip(texts, results):
     print(text)
     print(result)
